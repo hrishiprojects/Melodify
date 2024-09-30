d3253e8871c9d56623c69a4dc3892c7227ccef4b
Smudge <poms17226@gmail.com>
2024-09-30 00:15:38 -0400

    progressed with Adapter

--- app/src/main/java/com/example/musicapp/MyAdapter.kt
+++ app/src/main/java/com/example/musicapp/MyAdapter.kt
@@ -4,16 +4,18 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.LayoutInflater
 
 class MyAdapter(val context:Activity , val dataList: List<Data>):
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val itemView= LayoutInflater.from(context).inflate(R.layout.each_item, parent, false)
+        return MyViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
@@ -22,5 +24,18 @@ class MyAdapter(val context:Activity , val dataList: List<Data>):
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         TODO("Not yet implemented")
+    }
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val image: ImageView
+        val title: TextView
+        val play: ImageButton
+        val pause: ImageButton
+
+        init {
+            image = itemView.findViewById(R.id.musicImage)
+            title = itemView.findViewById(R.id.musicTitle)
+            play = itemView.findViewById(R.id.btnPlay)
+            pause = itemView.findViewById(R.id.btnPause)
+        }
     }
 }
